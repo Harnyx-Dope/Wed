569f94ac07152d19fd862d0e419115a93122cf71
HARSH VARDHAN GUPTA <85693353+Harnyx-Dope@users.noreply.github.com>
2022-06-20 18:27:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+ 
+
+function App() {
+  return (
+    <div className="App">
+       
+      <header className="App-header"> 
+      <div class="container" className='Happy'>
+       
+         
+         <h1 >
+            Welcome To Neha And Prateek Wedding 
+        </h1> 
+        <p class="lead" >
+            May You  Have A Healthy And Prosperous Life 
+          
+            Starting...
+            <h2>Welcome to a Happy Journey</h2>
+        </p> 
+            
+        </div>
+        <div class="container" className='hiissd'
+        >
+          <h1>Starting</h1>
+         
+        </div>
+        <div class="container" className='HVG1'> 
+        
+         <br></br>
+        
+         <iframe width="750" height="500" src="https://www.youtube.com/embed/atYf893pYRQ" 
+         title="YouTube video player" frameborder="0" allow="accelerometembed/https://youtu.be/r; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe> 
+    
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1>Beautiful Bride</h1>
+         
+        </div>
+       <div class="container" className='HVG2'>
+            <br></br>
+       <iframe width="750" height="500" src="https://www.youtube.com/embed/pniKO3P3oiY" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen" ></iframe> 
+    
+     
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1 >Haldi Started</h1>
+         
+        </div>
+       
+       <div class="container" className='HVG3'>
+           <br></br>
+       <iframe width="750" height="500" src="https://www.youtube.com/embed/4CtNeeHlF4I" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe> 
+    
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1 >Haldi Continue</h1>
+         
+        </div>
+       <div class="container" className='HVG4'>
+           <br></br>
+       <iframe width="750" height="500" src="https://www.youtube.com/embed/odvwYihUtlM" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen" ></iframe> 
+    
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1 >Mhendi..</h1>
+         
+        </div>
+       <div class="container" className='HVG5'>
+           <br></br>
+       <iframe width="750" height="500" src="https://www.youtube.com/embed/7hYzGQnhAgM" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen" ></iframe> 
+    
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1 >Happy-Happy</h1>
+         
+        </div>
+       <div class="container" className='HVG6'>
+           <br></br>
+       <iframe width="750" height="500" src="https://www.youtube.com/embed/tnMQmPIh5jI" 
+         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen" ></iframe> 
+    
+       </div>
+       <div class="container" className='hiissd'
+        >
+          <h1> Happy Family </h1>
+         
+        </div>
+       <div className='End'></div>
+
+       <div class="container" className='hiissd'
+        >
+          <h1>WISH YOU A VERY HAPPY AND PROSPEROUS LIFE
+
+            
+          </h1>
+           
+         
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
