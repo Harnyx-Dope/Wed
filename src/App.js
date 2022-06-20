3c7df43e679829bc0e25e318e0d34b6aee44073e
HARSH VARDHAN GUPTA <85693353+Harnyx-Dope@users.noreply.github.com>
2022-06-20 21:18:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <p class="lead" >
             May You  Have A Healthy And Prosperous Life 
           
-             Ahead..
+             Ahead....
             <h2>Welcome to a Happy Journey</h2>
         </p> 
             
